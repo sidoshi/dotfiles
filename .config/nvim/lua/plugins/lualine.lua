089b6fde06081817358dbad148097e476dba7c56
Siddharth Doshi <sidoshi510@gmail.com>
2026-02-10 14:51:26 +0100

    better lualine and stow

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,31 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    local function jj_branch()
+      if vim.fn.executable("jj") ~= 1 then
+        return ""
+      end
+
+      local cmd =
+        [[jj log -r @ --no-graph --ignore-working-copy --color never --limit 1 -T 'change_id.shortest(4) ++ " " ++ bookmarks' 2>/dev/null]]
+
+      local handle = io.popen(cmd)
+      if not handle then
+        return ""
+      end
+      local result = handle:read("*a")
+      handle:close()
+
+      local info = result:gsub("^%s+", ""):gsub("%s+$", "")
+      return info ~= "" and ("🥋 " .. info) or ""
+    end
+
+    table.insert(opts.sections.lualine_b, {
+      jj_branch,
+      cond = function()
+        return vim.fn.isdirectory(".jj") == 1
+      end,
+      color = { fg = "#ff9e64", gui = "bold" },
+    })
+  end,
+}
