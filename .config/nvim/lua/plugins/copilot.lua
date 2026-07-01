f90994874648cd1473b3ae86bfa3291beb603b7c
Siddharth Doshi <sidoshi510@gmail.com>
2026-07-01 16:34:36 +0200

    copilot

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept_word = "<M-w>", -- Change this to your preferred keymap (e.g., Alt + w)
+        },
+      },
+    },
+  },
+}
--- .config/nvim/lua/plugins/supermaven.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "supermaven-inc/supermaven-nvim",
-    cond = function()
-      local cwd = vim.fn.getcwd()
-      return not vim.startswith(cwd, vim.fn.expand("~/forto"))
-    end,
-  },
-}
